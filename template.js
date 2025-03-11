8bef3332bc78486466ba3c75bcd509f74e155732
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2025-03-11 18:39:26 +1000

    stage other files for button

--- template.js
+++ template.js
@@ -139,12 +139,12 @@ class Thumbnail extends HTMLElement {
       imageSrc: "default-image.jpg",
       header: "Default Header",
       subHeader: "Default SubHeader",
-      banner: "&nbsp",
+      banner: "&nbsp;",
       primaryLink: "#",
       primaryThumbnail: "Primary",
       secondaryLink: "#",
       secondaryThumbnail: "Secondary",
-      thumbnailColor: "#000000",
+      thumbnailColor: "--black",
       bodySize: "20px",
       titleSize: "24px",
       bannerSize: "16px",
@@ -179,16 +179,13 @@ class Thumbnail extends HTMLElement {
     overlay.className = "thumbnail-overlay";
     overlay.style.display = "flex";
     overlay.style.position = "absolute";
-    overlay.style.top = "0";
-    overlay.style.left = "0";
     overlay.style.width = "100%";
     overlay.style.height = "100%";
 
-    // Map verticalHug values to justify-content values
     const verticalHug = this.getAttributeOrDefault("verticalhug");
     switch (verticalHug) {
       case "top":
-        overlay.style.justifyContent = "flex-start";
+        overlay.style.alignContent = "flex-start";
         break;
       case "bottom":
         overlay.style.justifyContent = "flex-end";
@@ -199,25 +196,28 @@ class Thumbnail extends HTMLElement {
         break;
     }
 
-    // Map horizontalHug values to align-items values
-    const horizontalHug = this.getAttributeOrDefault("horizontalhug");
-    switch (horizontalHug) {
-      case "left":
-        overlay.style.alignItems = "flex-start";
-        break;
-      case "right":
-        overlay.style.alignItems = "flex-end";
-        break;
-      case "center":
-      default:
-        overlay.style.alignItems = "center";
-        break;
-    }
-
     const textContainer = document.createElement("div");
     textContainer.className = "thumbnail-text-container";
     textContainer.style.display = "flex";
     textContainer.style.flexDirection = "column";
+
+    const horizontalHug = this.getAttributeOrDefault("horizontalhug");
+    textContainer.style.left = "50%";
+    switch (horizontalHug) {
+      case "left":
+        textContainer.style.transform = "translateX(-100%)";
+        textContainer.style.alignItems = "flex-start";
+        break;
+      case "right":
+        textContainer.style.transform = "translateX(0)";
+        textContainer.style.alignItems = "flex-end";
+        break;
+      case "center":
+        textContainer.style.transform = "translateX(-50%)";
+        textContainer.style.alignItems = "center";
+      default:
+        break;
+    }
 
     const img = document.createElement("img");
     img.className = "thumbnail-image";
@@ -239,18 +239,119 @@ class Thumbnail extends HTMLElement {
     const linkContainer = document.createElement("div");
     linkContainer.className = "thumbnail-link-container";
 
-    const primaryLink = document.createElement("a");
-    primaryLink.className = "thumbnail-link";
-    primaryLink.href = this.getAttributeOrDefault("primaryLink");
-    primaryLink.innerHTML = `<p class="primaryThumbnail" style="--color:${this.getAttributeOrDefault("thumbnailColor")}; font-size:${this.getAttributeOrDefault("bodySize")}">${this.getAttributeOrDefault("primaryThumbnail")}<p>`;
+    const primaryThumbnailText = this.getAttribute("primaryThumbnail");
+    if (primaryThumbnailText) {
+      const primaryThumbnail = document.createElement("a");
+      primaryThumbnail.className = "thumbnail-link";
+      primaryThumbnail.href = this.getAttributeOrDefault("primaryLink");
+      // primaryThumbnail.style.display = "flex"; // Make it match flex properties
+      // primaryThumbnail.style.alignItems = "center"; // Center alignment like secondary
+      // primaryThumbnail.style.justifyContent = "center";
+      // primaryThumbnail.style.height = "100%"; // Ensure it stretches based on content
 
-    const secondaryLink = document.createElement("a");
-    secondaryLink.className = "thumbnail-link";
-    secondaryLink.href = this.getAttributeOrDefault("secondaryLink");
-    secondaryLink.innerHTML = `<p class="secondaryThumbnail" style="--color:${this.getAttributeOrDefault("thumbnailColor")}; font-size:${this.getAttributeOrDefault("bodySize")}">${this.getAttributeOrDefault("secondaryThumbnail")}<p>`;
+      // Get font size and color
+      const fontSize = this.getAttributeOrDefault("bodySize");
+      const thumbnailColor = this.getAttributeOrDefault("thumbnailColor");
 
-    linkContainer.appendChild(primaryLink);
-    linkContainer.appendChild(secondaryLink);
+      // Create SVG
+      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      svg.style.display = "block"; // Prevent extra spacing
+      svg.setAttribute("width", "100");
+      svg.setAttribute("height", "50");
+      svg.setAttribute("viewBox", "0 0 100 50");
+
+      // Create defs and mask
+      const defs = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "defs",
+      );
+      const mask = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "mask",
+      );
+      mask.setAttribute("id", "cutout-mask");
+
+      // Background for mask
+      const maskBackground = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "rect",
+      );
+      maskBackground.setAttribute("width", "100%");
+      maskBackground.setAttribute("height", "100%");
+      maskBackground.setAttribute("fill", "white");
+
+      // Text Mask (cutout effect)
+      const textMask = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "text",
+      );
+      textMask.setAttribute("x", "50%");
+      textMask.setAttribute("y", "50%");
+      textMask.setAttribute("dominant-baseline", "middle");
+      textMask.setAttribute("text-anchor", "middle");
+      textMask.setAttribute("font-size", fontSize);
+      textMask.setAttribute("fill", "black"); // Black makes it transparent in the mask
+      textMask.textContent = primaryThumbnailText;
+
+      mask.appendChild(maskBackground);
+      mask.appendChild(textMask);
+      defs.appendChild(mask);
+      svg.appendChild(defs);
+
+      // Background rect (thumbnail color)
+      const bgRect = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "rect",
+      );
+      bgRect.setAttribute("width", "100%");
+      bgRect.setAttribute("height", "100%");
+      bgRect.setAttribute("fill", thumbnailColor); // Use thumbnail color
+      bgRect.setAttribute("mask", "url(#cutout-mask)");
+
+      svg.appendChild(bgRect);
+      const svgWrapper = document.createElement("div");
+      svgWrapper.style.width = "100%";
+      svgWrapper.style.height = "fit-content";
+      svgWrapper.style.display = "block";
+      svgWrapper.style.margin = "auto";
+      svgWrapper.appendChild(svg);
+      primaryThumbnail.appendChild(svgWrapper);
+      linkContainer.appendChild(primaryThumbnail);
+
+      // Function to update size dynamically
+      function updateSvgSize() {
+        requestAnimationFrame(() => {
+          let textBounds = textMask.getBBox();
+
+          let padBounds = {
+            width: textBounds.width + 6,
+            height: textBounds.height + 6,
+          };
+
+          svg.setAttribute("width", padBounds.width);
+          svg.setAttribute("height", padBounds.height);
+          svg.setAttribute(
+            "viewBox",
+            `0 0 ${padBounds.width} ${padBounds.height}`,
+          );
+
+          primaryThumbnail.style.height = `${padBounds.height}px`;
+        });
+      }
+
+      // Ensure correct size after fonts load
+      document.fonts.ready.then(updateSvgSize);
+      window.addEventListener("resize", updateSvgSize);
+    }
+
+    const secondaryThumbnail = this.getAttribute("secondaryThumbnail");
+    if (secondaryThumbnail) {
+      const secondaryLink = document.createElement("a");
+      secondaryLink.className = "thumbnail-link";
+      secondaryLink.href = this.getAttributeOrDefault("secondaryLink");
+      secondaryLink.innerHTML = `<p class="secondaryThumbnail" style="--color:${this.getAttributeOrDefault("thumbnailColor")}; font-size:${this.getAttributeOrDefault("bodySize")}">${secondaryThumbnail}</p>`;
+      linkContainer.appendChild(secondaryLink);
+    }
 
     textContainer.appendChild(banner);
     textContainer.appendChild(header);
