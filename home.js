eeed2ddd4fe2acace1e7511f32ed93576128434b
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2025-03-29 18:27:52 +1000

    Some thumbnails broke

---
+++ home.js
@@ -0,0 +1,4 @@
+let thumbnails = [];
+thumbnails.push(new Thumbnail('Bamboo.png'));
+var grid = new Grid();
+grid.appendChild(thumbnails[0].element);
--- template.js
+++ template.js
@@ -1,8 +1,29 @@
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = "styling.css";
+var script1 = document.createElement("script");
+script1.src = "thumbnail.js";
+document.head.appendChild(script1);
 document.head.appendChild(link);
 
+// class TahananElement extends HTMLElement {
+//   getAttributeOrDefault(attr) {
+//     return this.getAttribute(attr) || this.defaultValues[attr];
+//   }
+//   attributeChangedCallback(name, oldValue, newValue) {
+//     if (oldValue !== newValue) {
+//       this.render();
+//     }
+//   }
+//   connectedCallback() {
+//     this.render();
+//   }
+//   createConstructor(){
+//     observedAttributes = this.constructor.observedAttributes;
+//     parameterString = "constructor(".concat(observedAttributes.join(", ")).concat("){");
+
+//   }
+// }
 // Define the topbar element using the modern approach
 class Topbar extends HTMLElement {
   static get observedAttributes() {
@@ -112,49 +133,9 @@ class ContentScroll extends HTMLElement {
 }
 
 class Footer extends HTMLElement {}
-
-class Thumbnail extends HTMLElement {
+class BigText extends HTMLElement {
   static get observedAttributes() {
-    return [
-      "imageSrc",
-      "header",
-      "subHeader",
-      "banner",
-      "primaryLink",
-      "primaryThumbnail",
-      "secondaryLink",
-      "secondaryThumbnail",
-      "thumbnailColor",
-      "bodySize",
-      "titleSize",
-      "bannerSize",
-      "horizontalhug",
-      "verticalhug",
-    ];
-  }
-
-  constructor() {
-    super();
-    this.defaultValues = {
-      imageSrc: "default-image.jpg",
-      header: "Default Header",
-      subHeader: "Default SubHeader",
-      banner: "&nbsp;",
-      primaryLink: "#",
-      primaryThumbnail: "Primary",
-      secondaryLink: "#",
-      secondaryThumbnail: "Secondary",
-      thumbnailColor: "--black",
-      bodySize: "20px",
-      titleSize: "24px",
-      bannerSize: "16px",
-      horizontalhug: "center",
-      verticalhug: "center",
-    };
-  }
-
-  getAttributeOrDefault(attr) {
-    return this.getAttribute(attr) || this.defaultValues[attr];
+    return ["text", "alignment"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -173,205 +154,147 @@ class Thumbnail extends HTMLElement {
     }
 
     const container = document.createElement("div");
-    container.className = "thumbnail-container";
+    container.className = "big-header-container";
+    container.style.maxWidth = "75%";
+    container.style.position = "relative";
+    container.style.top = "0";
+    container.style.bottom = "0";
+    container.style.display = "flex";
+    container.style.alignItems = "center";
+    container.style.margin = "30px;";
+    container.style.justifyContent = "flex-start";
+    container.style.padding = "30px";
+    container.style.boxSizing = "border-box";
 
-    const overlay = document.createElement("div");
-    overlay.className = "thumbnail-overlay";
-    overlay.style.display = "flex";
-    overlay.style.position = "absolute";
-    overlay.style.width = "100%";
-    overlay.style.height = "100%";
-
-    const verticalHug = this.getAttributeOrDefault("verticalhug");
-    switch (verticalHug) {
-      case "top":
-        overlay.style.alignContent = "flex-start";
-        break;
-      case "bottom":
-        overlay.style.justifyContent = "flex-end";
-        break;
-      case "center":
-      default:
-        overlay.style.justifyContent = "center";
-        break;
+    const alignment = this.getAttribute("alignment") || "left";
+    if (alignment === "right") {
+      container.style.right = "0";
+    } else {
+      container.style.left = "0";
     }
 
-    const textContainer = document.createElement("div");
-    textContainer.className = "thumbnail-text-container";
-    textContainer.style.display = "flex";
-    textContainer.style.flexDirection = "column";
-
-    const horizontalHug = this.getAttributeOrDefault("horizontalhug");
-    textContainer.style.left = "50%";
-    switch (horizontalHug) {
-      case "left":
-        textContainer.style.transform = "translateX(-100%)";
-        textContainer.style.alignItems = "flex-start";
-        break;
-      case "right":
-        textContainer.style.transform = "translateX(0)";
-        textContainer.style.alignItems = "flex-end";
-        break;
-      case "center":
-        textContainer.style.transform = "translateX(-50%)";
-        textContainer.style.alignItems = "center";
-      default:
-        break;
-    }
-
-    const img = document.createElement("img");
-    img.className = "thumbnail-image";
-    img.src = this.getAttributeOrDefault("imageSrc");
-
-    const header = document.createElement("h3");
-    header.textContent = this.getAttributeOrDefault("header");
-    header.style.fontSize = this.getAttributeOrDefault("titleSize");
-
-    const subHeader = document.createElement("p");
-    subHeader.textContent = this.getAttributeOrDefault("subHeader");
-    subHeader.style.fontSize = this.getAttributeOrDefault("bodySize");
-
-    const banner = document.createElement("p");
-    banner.textContent = this.getAttributeOrDefault("banner");
-    banner.style.color = "var(--yellow)";
-    banner.style.fontSize = this.getAttributeOrDefault("bannerSize");
-
-    const linkContainer = document.createElement("div");
-    linkContainer.className = "thumbnail-link-container";
-
-    const primaryThumbnailText = this.getAttribute("primaryThumbnail");
-    if (primaryThumbnailText) {
-      const primaryThumbnail = document.createElement("a");
-      primaryThumbnail.className = "thumbnail-link";
-      primaryThumbnail.href = this.getAttributeOrDefault("primaryLink");
-      // primaryThumbnail.style.display = "flex"; // Make it match flex properties
-      // primaryThumbnail.style.alignItems = "center"; // Center alignment like secondary
-      // primaryThumbnail.style.justifyContent = "center";
-      // primaryThumbnail.style.height = "100%"; // Ensure it stretches based on content
-
-      // Get font size and color
-      const fontSize = this.getAttributeOrDefault("bodySize");
-      const thumbnailColor = this.getAttributeOrDefault("thumbnailColor");
-
-      // Create SVG
-      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-      svg.style.display = "block"; // Prevent extra spacing
-      svg.setAttribute("width", "100");
-      svg.setAttribute("height", "50");
-      svg.setAttribute("viewBox", "0 0 100 50");
-
-      // Create defs and mask
-      const defs = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "defs",
+    const text = document.createElement("h1");
+    text.textContent = this.getAttribute("text") || "Default Big Header";
+    const updateFontSize = () => {
+      const vw = Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0,
       );
-      const mask = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "mask",
+      const vh = Math.max(
+        document.documentElement.clientHeight || 0,
+        window.innerHeight || 0,
       );
-      mask.setAttribute("id", "cutout-mask");
+      text.style.fontSize = `${vw * vh * 0.0001}px`;
+    };
 
-      // Background for mask
-      const maskBackground = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "rect",
-      );
-      maskBackground.setAttribute("width", "100%");
-      maskBackground.setAttribute("height", "100%");
-      maskBackground.setAttribute("fill", "white");
-
-      // Text Mask (cutout effect)
-      const textMask = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "text",
-      );
-      textMask.setAttribute("x", "50%");
-      textMask.setAttribute("y", "50%");
-      textMask.setAttribute("dominant-baseline", "middle");
-      textMask.setAttribute("text-anchor", "middle");
-      textMask.setAttribute("font-size", fontSize);
-      textMask.setAttribute("fill", "black"); // Black makes it transparent in the mask
-      textMask.textContent = primaryThumbnailText;
-
-      mask.appendChild(maskBackground);
-      mask.appendChild(textMask);
-      defs.appendChild(mask);
-      svg.appendChild(defs);
-
-      // Background rect (thumbnail color)
-      const bgRect = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "rect",
-      );
-      bgRect.setAttribute("width", "100%");
-      bgRect.setAttribute("height", "100%");
-      bgRect.setAttribute("fill", thumbnailColor); // Use thumbnail color
-      bgRect.setAttribute("mask", "url(#cutout-mask)");
-
-      svg.appendChild(bgRect);
-      const svgWrapper = document.createElement("div");
-      svgWrapper.style.width = "100%";
-      svgWrapper.style.height = "fit-content";
-      svgWrapper.style.display = "block";
-      svgWrapper.style.margin = "auto";
-      svgWrapper.appendChild(svg);
-      primaryThumbnail.appendChild(svgWrapper);
-      linkContainer.appendChild(primaryThumbnail);
-
-      // Function to update size dynamically
-      function updateSvgSize() {
-        requestAnimationFrame(() => {
-          let textBounds = textMask.getBBox();
-
-          let padBounds = {
-            width: textBounds.width + 6,
-            height: textBounds.height + 6,
-          };
-
-          svg.setAttribute("width", padBounds.width);
-          svg.setAttribute("height", padBounds.height);
-          svg.setAttribute(
-            "viewBox",
-            `0 0 ${padBounds.width} ${padBounds.height}`,
-          );
-
-          primaryThumbnail.style.height = `${padBounds.height}px`;
-        });
-      }
-
-      // Ensure correct size after fonts load
-      document.fonts.ready.then(updateSvgSize);
-      window.addEventListener("resize", updateSvgSize);
-    }
-
-    const secondaryThumbnail = this.getAttribute("secondaryThumbnail");
-    if (secondaryThumbnail) {
-      const secondaryLink = document.createElement("a");
-      secondaryLink.className = "thumbnail-link";
-      secondaryLink.href = this.getAttributeOrDefault("secondaryLink");
-      secondaryLink.innerHTML = `<p class="secondaryThumbnail" style="--color:${this.getAttributeOrDefault("thumbnailColor")}; font-size:${this.getAttributeOrDefault("bodySize")}">${secondaryThumbnail}</p>`;
-      linkContainer.appendChild(secondaryLink);
-    }
-
-    textContainer.appendChild(banner);
-    textContainer.appendChild(header);
-    textContainer.appendChild(subHeader);
-    textContainer.appendChild(linkContainer);
-
-    overlay.appendChild(textContainer);
-    container.appendChild(img);
-    container.appendChild(overlay);
-
+    updateFontSize();
+    window.addEventListener("resize", updateFontSize);
+    text.style.whiteSpace = "normal"; // Allow text to wrap
+    container.appendChild(text);
     this.appendChild(container);
   }
 }
 
+// A carousel element that loops through a list of thumbnails
+class Carousel extends HTMLElement {
+  // there are no observed attributes, just innnerHTML with thubmnail tags
+  connectedCallback() {
+    this.render();
+  }
+  render() {
+    let thumbnails = this.innerHTML;
+    this.innerHTML = "";
+    let carousel = document.createElement("div");
+  }
+}
+// a grid element that displays thumbnails in a grid
+class Grid extends HTMLElement {
+  static get observedAttributes() {
+    return ["columns", "rows", "gap", "width", "height", "contents"];
+  }
+  constructor() {
+    super();
+    this.defaultValues = {
+      columns: 3,
+      rows: 3,
+      gap: "10px",
+      width: "100%",
+      height: "100%",
+    };
+  }
+  getAttributeOrDefault(attr) {
+    return this.getAttribute(attr) || this.defaultValues[attr];
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.render();
+    }
+  }
+  connectedCallback() {
+    this.contents = this.innerHTML;
+    this.render();
+  }
+  // scanForThumbnails(element) {
+  //   console.log(element);
+  //   let returner = [];
+  //   while (element.firstChild) {
+  //     console.log("hello");
+  //     if (element.firstChild.tagName === "tahanan-thumbnail") {
+  //       returner.push(element.firstChild);
+  //     } else {
+  //       returner = returner.concat(this.scanForThumbnails(element.firstChild));
+  //     }
+  //     element.removeChild(element.firstChild);
+  //   }
+  //   return returner;
+  // }
+  render() {
+    while (this.firstChild) {
+      this.removeChild(this.firstChild);
+    }
+    let thumbnails = this.getAttribute("contents");
+    console.log(thumbnails);
+    const container = document.createElement("div");
+    container.className = "grid-container";
+    container.style.display = "grid";
+    container.style.gridTemplateColumns = `repeat(${this.getAttributeOrDefault("columns")}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${this.getAttributeOrDefault("rows")}, 1fr)`;
+    container.style.gap = this.getAttributeOrDefault("gap");
+    container.style.width = this.getAttributeOrDefault("width");
+    container.style.height = this.getAttributeOrDefault("height");
+
+    for (let i = 0; i < thumbnails; i++) {
+      const wrapper = document.createElement("div");
+      wrapper.style.width = "100%";
+      wrapper.style.height = "100%";
+      wrapper.style.display = "flex";
+      wrapper.style.alignItems = "center";
+      wrapper.style.justifyContent = "center";
+      wrapper.appendChild(thumbnails[i]);
+      container.appendChild(wrapper);
+    }
+    this.appendChild(container);
+  }
+}
+
+// tahanan-loader
+// class Loader extends HTMLElement{
+//   connectedCallback(){
+//     this.render();
+//   }
+//   // no attributes, so no need for attributeChangedCallback, or refreshing the render
+//   render(){
+//     let loader
+//   }
+// }
+
+customElements.define("big-header", BigText);
 // Register the custom elements
 customElements.define("tahanan-topbar", Topbar);
 customElements.define("tahanan-content-scroll", ContentScroll);
 customElements.define("tahanan-footer", Footer);
-customElements.define("tahanan-thumbnail", Thumbnail);
-
+customElements.define("tahanan-carousel", Carousel);
+customElements.define("tahanan-grid", Grid);
 function addAllElements() {
   // Elements are already in the DOM, no need to append them again
   document.querySelectorAll("tahanan-content-scroll").forEach((element) => {
